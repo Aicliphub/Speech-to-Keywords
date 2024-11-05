e0dbef48e3a6b5985c8825da13d27ad5e34ad14c
Aicliphub <aicliphub@gmail.com>
2024-11-05 22:53:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*']
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Define a request model
